bebfb4c744ab802fdf6eaec19fb6662e68429662
𝕆𝕝𝕒😈🦍 <basitsile2005@gmail.com>
2024-08-26 09:31:49 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+
+
 // Components
 const vuetify = createVuetify({
     components,
